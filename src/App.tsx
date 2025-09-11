3ae40f2a8c86ec291042ccd5d438dbe7621ee60a
gin <73906888+gingkapls@users.noreply.github.com>
2025-09-11 13:15:17 +0530

    add bounding-boxes to scanImage, add driver code

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,27 @@
 import { useEffect, useRef, useState } from "react";
-import wishTest from "./assets/wish-test.jpg";
+import wishTest from "./assets/full-wish.webp";
 import "./App.css";
 import { scanImage } from "./lib/scanImages.ts";
-import { processImage } from "./lib/convertImage.ts";
+import { processImage } from "./lib/processImage.ts";
+import Tesseract from "tesseract.js";
 
 function App() {
-  const [data, setData] = useState("");
+  const [data, setData] = useState<Tesseract.Block[]>([]);
   const grayImageRef = useRef(null);
   const imageRef = useRef(null);
-
-  useEffect(() => {
-    if (grayImageRef.current === null) return;
-    if (imageRef.current === null) return;
-    console.log(grayImageRef.current);
-
-    scanImage(imageRef.current.src).then(data => {
-      setData(data);
-    });
-  }, [data]);
 
   useEffect(() => {
     async function doStuff() {
       if (grayImageRef.current === null || imageRef.current === null) return;
 
       await processImage(imageRef.current, grayImageRef.current);
+      if (grayImageRef.current === null) return;
+      if (imageRef.current === null) return;
+      console.log(grayImageRef.current);
+
+      scanImage(grayImageRef.current).then((data) => {
+        if (data.blocks) setData(data.blocks);
+      });
     }
 
     doStuff();
@@ -34,7 +32,7 @@ function App() {
       <h1>Wish test</h1>
       <img ref={imageRef} src={wishTest} alt="Sample"></img>
       <canvas ref={grayImageRef} />
-      <pre>data: {data}</pre>
+      <pre>{data.map(block => block.text).join('')}</pre>
     </>
   );
 }
--- src/lib/scanImages.ts
+++ src/lib/scanImages.ts
@@ -1,17 +1,71 @@
-import { createWorker, PSM } from "tesseract.js";
+import Tesseract, { createWorker, PSM } from "tesseract.js";
 
-const rectangle = {
-  top: 640,
-  left: 761,
-  height: 91,
-  width: 341,
-} as const;
+// Bounding Box
+interface bbox {
+  TOP_RATIO: number;
+  LEFT_RATIO: number;
+  WIDTH_RATIO: number;
+  HEIGHT_RATIO: number;
+}
 
-export async function scanImage(imageSrc: string) {
+const TOP_RATIO = 0.29;
+const HEIGHT_RATIO = 0.44;
+
+const ITEM_TYPE_BBOX: bbox = {
+  TOP_RATIO,
+  LEFT_RATIO: 0.18,
+  WIDTH_RATIO: 0.09,
+  HEIGHT_RATIO,
+};
+
+const ITEM_NAME_BBOX: bbox = {
+  TOP_RATIO,
+  LEFT_RATIO: 0.28,
+  WIDTH_RATIO: 0.16,
+  HEIGHT_RATIO,
+};
+
+const WISH_TYPE_BBOX: bbox = {
+  TOP_RATIO,
+  LEFT_RATIO: 0.43,
+  WIDTH_RATIO: 0.17,
+  HEIGHT_RATIO,
+};
+
+const TIME_RECEIVED_BBOX: bbox = {
+  TOP_RATIO,
+  LEFT_RATIO: 0.59,
+  WIDTH_RATIO: 0.19,
+  HEIGHT_RATIO,
+};
+
+function getRectangle(
+  bbox: bbox,
+  image: { width: number; height: number }
+): Tesseract.Rectangle {
+  const top = TOP_RATIO * image.height;
+  const height = HEIGHT_RATIO * image.height;
+
+  return {
+    top,
+    left: bbox.LEFT_RATIO * image.width,
+    width: bbox.WIDTH_RATIO * image.width,
+    height,
+  };
+}
+
+export async function scanImage(image: HTMLCanvasElement) {
   const worker = await createWorker("eng");
-  worker.setParameters({tessedit_pageseg_mode: PSM.SINGLE_BLOCK})
-  const ret = await worker.recognize(imageSrc, { rectangle});
+  worker.setParameters({
+    tessedit_pageseg_mode: PSM.SINGLE_COLUMN,
+    preserve_interword_spaces: "1",
+  });
+  const ret = await worker.recognize(
+    image,
+    { rectangle: getRectangle(WISH_TYPE_BBOX, image) },
+    { blocks: true, text: false }
+  );
   await worker.terminate();
-  console.log(ret.data.text);
-  return ret.data.text;
+  console.log(ret.data);
+  return ret.data;
 }
