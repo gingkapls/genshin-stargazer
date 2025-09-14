20fd0d72843a695c25003b246f16634525294d53
gin <73906888+gingkapls@users.noreply.github.com>
2025-09-14 22:25:23 +0530

    processImage: minor changes

--- src/lib/processImage.ts
+++ src/lib/processImage.ts
@@ -9,21 +9,11 @@ export async function processImage(
     const src = cv.imread(input);
     const dst = new cv.Mat();
 
-    const color = new cv.Scalar(0, 0, 0); // red
-
-    // cv.cvtColor(src, src, cv.COLOR_RGBA2RGB);
-    // cv.bilateralFilter(src, dst, 9, 75, 75, cv.BORDER_DEFAULT);
+    // Resizing for clearer OCR
+    cv.resize(src, src, new cv.Size(1920, 1080), 0, 0, cv.INTER_AREA);
 
     // Grayscaling
-    // cv.resize(src, src, new cv.Size(1920, 1080), 0, 0, cv.INTER_AREA);
-
     cv.cvtColor(src, dst, cv.COLOR_BGR2GRAY);
-
-    // const M = cv.Mat.ones(1, 1, cv.CV_8U);
-    // const anchor = new cv.Point(-1, -1);
-    // You can try more different parameters
-    // cv.erode(dst, dst, cv.Mat.ones(1, 1, cv.CV_8U), anchor, 1, cv.BORDER_CONSTANT, cv.morphologyDefaultBorderValue());
-    // cv.dilate(dst, dst, M, anchor, 1, cv.BORDER_CONSTANT, cv.morphologyDefaultBorderValue());
 
     // Thresholding
     cv.threshold(dst, dst, 181, 255, cv.THRESH_BINARY);
@@ -49,25 +39,15 @@ export async function processImage(
       maxY = Math.max(maxY, lines.data32S[i * 4 + 3]);
     }
 
-    // let border = cv.Mat.zeros(dst.rows, dst.cols, cv.CV_8UC3);
-
-    // Horizontals
-    // cv.line(dst, new cv.Point(minX, minY), new cv.Point(maxX, minY), color, 10);
-    // cv.line(dst, new cv.Point(minX, maxY), new cv.Point(maxX, maxY), color, 10);
-
-    // Verticals
-    // cv.line(dst, new cv.Point(minX, minY), new cv.Point(minX, maxY), color, 10);
-    // cv.line(dst, new cv.Point(maxX, minY), new cv.Point(maxX, maxY), color, 10);
-    //
     const height = maxY - minY;
     const width = maxX - minX;
 
-    // cv.rectangle(dst, new cv.Point(0, 0), new cv.Point(120, 120), [0, 0, 255, 255]);
     cv.imshow(output, dst);
 
     // release resources
     src.delete();
     dst.delete();
+
     return {
       top: minY,
       left: minX,
