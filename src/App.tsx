d667cd0646f6e53bc666b13877ad360cc6c1a539
gin <73906888+gingkapls@users.noreply.github.com>
2025-09-17 00:32:45 +0530

    Refactor: Lift wish history state up to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,60 @@
+import { useReducer } from "react";
 import "./App.css";
 import FolderPicker from "./components/FolderPicker.tsx";
+import type { WishHistory } from "./components/wishHistory";
+import type { parsedHistoryPage } from "./lib/parseData.ts";
+
+// TODO: move somewhere better
+function convertToKey(
+  wishType: parsedHistoryPage["wishType"]
+): keyof WishHistory {
+  return wishType
+    .toLowerCase()
+    .split(" ")
+    .join("_")
+    .replaceAll("-", "_") as keyof WishHistory;
+}
+
+function reducer(state: WishHistory, action: { pages: parsedHistoryPage[] }) {
+  // TODO: make it more readable maybe?
+  return action.pages.reduce<WishHistory>(
+    (acc, cur) => {
+      const wishType = convertToKey(cur.wishType);
+
+      const hasPage: boolean =
+        acc[wishType].findIndex(({ pageHash }) => cur.pageHash === pageHash) !==
+        -1;
+
+      // TODO: Implement wish merging algorithm
+      // to not add duplicates
+      acc[wishType] = hasPage ? acc[wishType] : acc[wishType].concat(cur);
+
+      console.log({ hasPage, page: cur });
+
+      return acc;
+    },
+    { ...state }
+  );
+}
 
 // TODO: Implement LocalStorage
 function App() {
+  const [data, dispatch] = useReducer<
+    WishHistory,
+    [Parameters<typeof reducer>[1]]
+  >(reducer, {
+    character_event_wish: [],
+    character_event_wish_2: [],
+    weapon_event_wish: [],
+    permanent_wish: [],
+    chronicled_wish: [],
+  });
+
+  console.log("App: ", { data });
+
   return (
     <>
-      <FolderPicker />
+      <FolderPicker dispatch={dispatch} />
     </>
   );
 }
--- src/components/FolderPicker.tsx
+++ src/components/FolderPicker.tsx
@@ -1,60 +1,21 @@
-import { useReducer, useState, type ChangeEvent } from "react";
+import {
+  useReducer,
+  useState,
+  type ActionDispatch,
+  type ChangeEvent,
+  type Dispatch,
+  type SetStateAction,
+} from "react";
 import Scanner from "./Scanner.tsx";
 import type { parsedHistoryPage } from "../lib/parseData.ts";
-import type { WishHistory } from "./wishHistory.ts";
 import type { WishImage } from "./wishImage";
 
-
-// TODO: move somewhere better
-function convertToKey(
-  wishType: parsedHistoryPage["wishType"]
-): keyof WishHistory {
-  return wishType
-    .toLowerCase()
-    .split(" ")
-    .join("_")
-    .replaceAll("-", "_") as keyof WishHistory;
+interface FolderPickerProps {
+  dispatch: ActionDispatch<[{ pages: parsedHistoryPage[] }]>;
 }
 
-function reducer(state: WishHistory, action: { pages: parsedHistoryPage[] }) {
-  // TODO: Implement hashing
-  // TODO: make it more readable maybe?
-  return action.pages.reduce<WishHistory>(
-    (acc, cur) => {
-      const wishType = convertToKey(cur.wishType);
-
-      // TODO: Change pageNumber to hash
-      const hasPage: boolean =
-        acc[wishType].findIndex(
-          ({ pageNumber }) => cur.pageNumber === pageNumber
-        ) === -1;
-
-      return {
-        ...acc,
-        ...(hasPage && { [wishType]: acc[wishType].concat(cur) }),
-      };
-    },
-    {
-      ...state,
-    }
-  );
-}
-
-function FolderPicker() {
+function FolderPicker({ dispatch }: FolderPickerProps) {
   const [images, setImages] = useState<WishImage[]>([]);
-
-  const [data, dispatch] = useReducer<
-    WishHistory,
-    [Parameters<typeof reducer>[1]]
-  >(reducer, {
-    character_event_wish: [],
-    character_event_wish_2: [],
-    weapon_event_wish: [],
-    permanent_wish: [],
-    chronicled_wish: [],
-  });
-
-  console.log(data);
 
   function handleChange(e: ChangeEvent<HTMLInputElement>) {
     if (e.target.files) {
@@ -65,7 +26,8 @@ function FolderPicker() {
         return { src: i, hash } satisfies WishImage;
       });
 
-      // Deduplicate images
+      // Adding the previous images
+      // and deduplicate them before sending them to the scanner
       const uniqueImages = images.concat(
         newImages.filter(
           (image) => images.findIndex((i) => i.hash === image.hash) === -1
@@ -78,7 +40,7 @@ function FolderPicker() {
   return (
     <>
       <input type="file" multiple onChange={handleChange} />
-      <Scanner images={images} data={data} dispatch={dispatch} />
+      <Scanner images={images} dispatch={dispatch} />
     </>
   );
 }
